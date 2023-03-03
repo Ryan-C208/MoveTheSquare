4c495dab908f111ce7fa1094d4ed4f6dfb6d311f
Enoch Sam <esam@ycp.edu>
2023-03-03 11:04:09 -0500

    Removed glitchy boundaries

--- MoveTheSquare/src/edu/ycp/cs320/movethesquare/controllers/GameController.java
+++ MoveTheSquare/src/edu/ycp/cs320/movethesquare/controllers/GameController.java
@@ -12,43 +12,16 @@ public class GameController {
 			double moveX = 0, moveY = 0;
 			
 			if (dx > 0) {
-				if ((square.getX() + Game.MOVE_DIST) > 500.0)
-				{
-					moveX = 0.0;
-				}
-				else {
-					moveX = Game.MOVE_DIST;
-				}
+				moveX = Game.MOVE_DIST;
 			}
 			else {
-				if((square.getX() - Game.MOVE_DIST) < 10.0)
-				{
-					moveX = 0.0;
-				}
-				else
-				{
-					moveX = -Game.MOVE_DIST;
-				}
+				moveX = -Game.MOVE_DIST;
 			}
 			if (dy > 0) {
-				if((square.getY() + Game.MOVE_DIST) > 400.0)
-				{
-					moveY = 0.0;
-				}
-				else 
-				{
-					moveY = Game.MOVE_DIST;
-				}
+				moveY = Game.MOVE_DIST;
 			} 
 			else {
-				if((square.getY() - Game.MOVE_DIST) < 10)
-				{
-					moveY = 0.0;
-				}
-				else 
-				{
-					moveY = -Game.MOVE_DIST;
-				}
+				moveY = -Game.MOVE_DIST;
 			}
 			
 			game.setSquareDx(moveX);
@@ -57,7 +30,13 @@ public class GameController {
 	}
 
 	public void moveSquare(Game model, Square square) {
-		square.setX(square.getX() + model.getSquareDx());
-		square.setY(square.getY() + model.getSquareDy());
+		if((square.getX() + model.getSquareDx() > 0.0 && (square.getX() + model.getSquareDx()) < 640.0))
+		{
+			square.setX(square.getX() + model.getSquareDx());
+		}
+		if((square.getY() + model.getSquareDy()) > 0.0 && (square.getY() + model.getSquareDy()) < 480.0)
+		{
+			square.setY(square.getY() + model.getSquareDy());
+		}
 	}
 }
